f412aedff3438ee9f2f54b6a5d628f909e719312
aAg3 <80839591+aAg3@users.noreply.github.com>
2023-04-07 20:45:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,9 @@ from scipy.stats import norm
 chat_id = 348708372 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    s = x.std(ddof = 1)
-    return x.mean() + s * norm.ppf(alpha / 2) / np.sqrt(len(x)), \
-           x.mean() + s * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))
+    a = 0.095
+    alpha = 1 - p  
+    n = len(x)  
+    b = np.max(x)
+    
+    return a, b + (1 - np.sqrt(1 - alpha)) * (1 - b)
